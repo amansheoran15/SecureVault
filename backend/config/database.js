3a1f31c24c2d8168b95d8b9e4909857c77bf5b51
Aman <amansheo@gmail.com>
2024-05-30 00:11:55 +0530

    Add authentication APIs

---
+++ backend/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+exports.connectDatabase = () => {
+    mongoose.connect(process.env.MONGO_URI)
+        .then((con)=>{
+            console.log(`Connected to database : ${con.connection.host}`);
+        })
+        .catch((err)=>{
+            console.error(`Connection failed with error : ${err}`);
+        })
+}
---
+++ backend/middlewares/auth.js
@@ -0,0 +1,23 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+exports.authenticate = async (req,res,next)=>{
+    try{
+        const {token} = req.cookies;
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                msg: "Please log in"
+            })
+        }
+
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+        req.user_id = decoded.user_id;
+        next();
+    }catch (e) {
+        return res.status(500).json({
+            success: false,
+            msg: e
+        })
+    }
+}
