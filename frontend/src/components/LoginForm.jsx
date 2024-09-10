fdd6bb5fa9cf5f6fc838df41addf6396d8c08790
Aman <amansheo@gmail.com>
2024-09-10 15:51:10 +0530

    Add login form validations

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -5,26 +5,49 @@ import { HiMail } from "react-icons/hi";
 import { FaKey, FaGoogle } from "react-icons/fa";
 import SignInWithGoogle from "./SignInWithGoogle.jsx";
 import {Link} from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import {generateAKey, saveKey} from "./UtilityFunctions.jsx";
 
 export default function LoginForm(props) {
+    const { register, handleSubmit, formState : {errors}, getValues } = useForm();
+
+    const onSubmitCallback = async (formData) => {
+        console.log(formData);
+    };
+
     return<div className="flex flex-col items-center justify-center h-screen w-4/5 m-auto">
+
         <div id= "heading" className="text-center font-semibold text-4xl mb-5">
             MyLocker
         </div>
         <div id="form-container" className="w-full flex flex-col items-center">
-            <form className="flex max-w-md flex-col gap-4 w-full">
+            <form className="flex max-w-md flex-col gap-4 w-full" onSubmit={handleSubmit(onSubmitCallback)}>
                 <div >
                     <div className="mb-2 block">
                         <Label htmlFor="email1" value="Email"/>
                     </div>
-                    <TextInput id="email1" type="email" placeholder="abc@example.com" rightIcon={HiMail} required shadow/>
+                    <TextInput id="email1" {...register("email", {
+                        required: "This is a required field",
+                        pattern: {
+                            value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                            message: 'Invalid email address',
+                        },
+                    })} color={errors.email ? "failure" : undefined} helperText={errors.email ? <> {errors.email.message}</> : undefined} type="email" placeholder="abc@example.com" rightIcon={HiMail} shadow/>
                 </div>
                 <div>
                     <div className="mb-2 block">
                         <Label htmlFor="password1" value="Password"/>
                     </div>
-                    <TextInput className="rounded-md" id="password1" type="password" rightIcon={FaKey}
-                               placeholder="password123" required shadow/>
+                    <TextInput className="rounded-md" id="password1" {...register("password", {
+                        required: "This is a required field",
+                        minLength: {
+                            value: 8,
+                            message: "Password is at least 8 characters"
+                        }
+                    })} color={errors.password ? "failure" : undefined}
+                               helperText={errors.password ? <> {errors.password.message}</> : undefined} type="password" rightIcon={FaKey}
+                               placeholder="password123" shadow/>
                     <div id="forgot-password" className="text-right w-full max-w-md text-gray-500 text-sm mt-1">
                         <a className="cursor-pointer">Forgot Password?</a>
                     </div>
